4fe35185a3579d390e063869724dd1e4f07c36f8
ssssksss <ssssksss@gmail.com>
2022-02-26 21:12:03 +0900

    test auto jenkins deploy

--- src/main/java/com/ssssksss/ssssksss_server/TestController.java
+++ src/main/java/com/ssssksss/ssssksss_server/TestController.java
@@ -19,7 +19,7 @@ public class TestController {
 
     @RequestMapping("/")
     public String index() throws Exception{
-        return "Hello World2";
+        return "Hello World3";
     }
     @RequestMapping(value="/ssssksss/test", method= RequestMethod.GET)
     public String test() throws Exception{
